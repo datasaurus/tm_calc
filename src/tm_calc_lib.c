1251eb0e8e868b4dc104efb52b1d4b849492aad5
Gordon Carrie <dev0@trekix.net>
2009-07-08 21:44:48 +0000

    Replaced Julian date struct with double

--- src/tm_calc_lib.c
+++ src/tm_calc_lib.c
@@ -14,43 +14,45 @@
 	Communications of the ACM archive
 	Volume 11 , Issue 10 (October 1968) Page: 657 ISSN:0001-0782 
 
-   $Revision: $ $Date: $
+   $Revision: 1.1 $ $Date: 2009/07/07 21:44:08 $
  */
 
 #include <math.h>
+#include "err_msg.h"
 #include "tm_calc_lib.h"
 
-struct tm_jul tm_julset(int day, double second)
+int tm_caltojul(int year, int month, int day,
+	int hour, int minute, double second, double *julday)
 {
-    struct tm_jul jul;
+    double j1, j2;
 
-    jul.day = day;
-    jul.second = second;
-    return jul;
-}
-
-struct tm_jul tm_caltojul(int year, int month, int day,
-	int hour, int minute, double second)
-{
-    struct tm_jul jul;		/* Return value */
-
-    jul.day =   (1461 * (year + 4800 + (month - 14) / 12)) / 4
+    j1 =   (1461 * (year + 4800 + (month - 14) / 12)) / 4
 	+ (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12
 	- (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4
-	+ day - 32075;
-    jul.second = 3600.0 * hour + 60.0 * minute + second;
-    return jul;
+	+ day - 32075 + hour / 24.0 + minute / 1440.0 + second / 86400.0;
+    /* Check for inadequate resolution */
+    j2 = nextafter(j1, j1 + 1);
+    if (fabs(j1 - j2) * 86400.0 > TMCALC_RESOLUTION) {
+	err_append("Julian date to big for desired resolution.");
+	return 0;
+    }
+    *julday = j1;
+    return 1;
 }
 
-void tm_jultocal(struct tm_jul jul, int *year, int *month,
+int tm_jultocal(double julday, int *year, int *month,
 	int *day, int *hour, int *minute, double *second)
 {
+    double iday, fday;
     int l, n, i, j;		/* Intermediaries */
-    int h, m;			/* Hour, minute */
-    int s;			/* Second */
-    double fsec;
+    double ihour, fhour, imin, fmin;
 
-    l = jul.day + 68569;
+    fday = modf(julday, &iday);
+    if (iday > INT_MAX) {
+	err_append("Julian date too big for integer arithmetic.");
+	return 0;
+    }
+    l = (int)iday + 68569;
     n = (4 * l) / 146097;
     l = l - (146097 * n + 3) / 4;
     i = (4000 * (l + 1)) / 1461001;
@@ -61,56 +63,10 @@ void tm_jultocal(struct tm_jul jul, int *year, int *month,
     *month = j + 2 - (12 * l);
     *year = 100 * (n - 49) + i + l;
 
-    s = floor(jul.second);
-    fsec = jul.second - s;
-    h = s / 3600;
-    s %= 3600;
-    m = s / 60;
-    s %= 60;
-    if (h > 23) {
-	*day += h / 24;
-	h %= 24;
-    }
-    *hour = h;
-    *minute = m;
-    *second = s + fsec;
-}
-
-void tm_incr(struct tm_jul *jul, double ds)
-{
-    int d;
-    double s;
-
-    s = jul->second + ds;
-    d = floor(s / 86400.0);
-    jul->day += d;
-    jul->second = s - d * 86400.0;
-}
-
-int tm_cmp(struct tm_jul jul1, struct tm_jul jul2)
-{
-
-    jul1 = tm_julset(jul1.day, jul1.second);
-    jul2 = tm_julset(jul2.day, jul2.second);
-    if (jul1.day > jul2.day) {
-	return 1;
-    }
-    if (jul1.day < jul2.day) {
-	return -1;
-    }
-    if (jul1.second > jul2.second) {
-	return 1;
-    }
-    if (jul1.second < jul2.second) {
-	return -1;
-    }
-    return 0;
-}
-
-double tm_diff(struct tm_jul jul1, struct tm_jul jul2)
-{
-
-    jul1 = tm_julset(jul1.day, jul1.second);
-    jul2 = tm_julset(jul2.day, jul2.second);
-    return (jul1.day - jul2.day) * 86400.0 + jul1.second - jul2.second;
+    fhour = modf(fday * 24.0, &ihour);
+    *hour = (int)ihour;
+    fmin = modf(fhour * 60.0, &imin);
+    *minute = (int)imin;
+    *second = fmin * 60.0;
+    return 1;
 }
--- src/tm_calc_lib.h
+++ src/tm_calc_lib.h
@@ -9,24 +9,16 @@
 
    Please address questions and feedback to dev0@trekix.net.
 
-   @(#) $Id: $
+   @(#) $Id: tm_calc_lib.h,v 1.1 2009/07/07 21:43:29 gcarrie Exp $
  */
 
 #ifndef _TMCALC_H_
 #define _TMCALC_H_
 
-/* Time instant as a Julian day and seconds since midnight */
-struct tm_jul {
-    int   day;		/* Julian day */
-    double second;	/* Seconds since midnight */
-};
+/* Desired time resolution, in seconds */
+#define TMCALC_RESOLUTION 0.0001
 
-/* Global functions */
-struct tm_jul tm_julset(int, double);
-struct tm_jul tm_caltojul(int, int, int, int, int, double);
-void tm_jultocal(struct tm_jul, int *, int *, int *, int *, int *, double *);
-void tm_incr(struct tm_jul *, double);
-int tm_cmp(struct tm_jul, struct tm_jul);
-double tm_diff(struct tm_jul, struct tm_jul);
+int tm_caltojul(int, int, int, int, int, double, double *);
+int tm_jultocal(double, int *, int *, int *, int *, int *, double *);
 
 #endif
