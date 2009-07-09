51f2cb3a8686c9ccb66b53858ead2914dacff5c0
Gordon Carrie <dev0@trekix.net>
2009-07-09 15:47:28 +0000

    Added resoln subcommand

--- src/tm_calc_app.c
+++ src/tm_calc_app.c
@@ -7,7 +7,7 @@
   
    Please send feedback to dev0@trekix.net
 
-   $Revision: 1.2 $ $Date: 2009/07/08 19:10:34 $
+   $Revision: 1.3 $ $Date: 2009/07/08 21:47:06 $
  */
 
 #include <stdlib.h>
@@ -22,17 +22,18 @@ char *cmd, *cmd1;
 
 /* Callback functions.  There should be one for each subcommand. */
 typedef int (callback)(int , char **);
+callback resoln_cb;
 callback caltojul_cb;
 callback jultocal_cb;
 
 /* Number of subcommands */
-#define NCMD 2
+#define NCMD 3
 
 /* Array of subcommand names */
-char *cmd1v[NCMD] = {"caltojul", "jultocal"};
+char *cmd1v[NCMD] = {"resoln", "caltojul", "jultocal"};
 
 /* Array of subcomand callbacks. cb1v[i] is the callback for cmd1v[i] */
-callback *cb1v[NCMD] = {caltojul_cb, jultocal_cb};
+callback *cb1v[NCMD] = {resoln_cb, caltojul_cb, jultocal_cb};
 
 int main(int argc, char *argv[])
 {
@@ -66,6 +67,72 @@ int main(int argc, char *argv[])
     }
     return !rslt;
 }
+int resoln_cb(int argc, char *argv[])
+{
+    char *yr_s, *mo_s, *dy_s, *hr_s, *mi_s, *sc_s;
+    int yr, mo, dy, hr, mi;
+    double sc;
+    int da;
+    char *fmt;
+
+    /* Ensure minimum command line */
+    if (argc < 7) {
+	err_append("Usage: ");
+	err_append(cmd);
+	err_append(" ");
+	err_append(cmd1);
+	err_append(" [-f format] year month day hour minute second\n");
+	return 0;
+    }
+    fmt = "%lf\n";
+    da = 0;
+    if (strcmp(argv[1], "-f") == 0) {
+	fmt = stresc(argv[2]);
+	da = 2;
+    }
+    yr_s = argv[1 + da];
+    mo_s = argv[2 + da];
+    dy_s = argv[3 + da];
+    hr_s = argv[4 + da];
+    mi_s = argv[5 + da];
+    sc_s = argv[6 + da];
+
+    /* Get values from command line arguments */
+    if (sscanf(yr_s, "%d", &yr) != 1) {
+	err_append("Expected integer value for year, got ");
+	err_append(yr_s);
+	return 0;
+    }
+    if (sscanf(mo_s, "%d", &mo) != 1) {
+	err_append("Expected integer value for month, got ");
+	err_append(mo_s);
+	return 0;
+    }
+    if (sscanf(dy_s, "%d", &dy) != 1) {
+	err_append("Expected integer value for day, got ");
+	err_append(dy_s);
+	return 0;
+    }
+    if (sscanf(hr_s, "%d", &hr) != 1) {
+	err_append("Expected integer value for hour, got ");
+	err_append(hr_s);
+	return 0;
+    }
+    if (sscanf(mi_s, "%d", &mi) != 1) {
+	err_append("Expected integer value for minute, got ");
+	err_append(mi_s);
+	return 0;
+    }
+    if (sscanf(sc_s, "%lf", &sc) != 1) {
+	err_append("Expected float value for second, got ");
+	err_append(sc_s);
+	return 0;
+    }
+
+    /* Send result */
+    printf(fmt, tm_resoln(yr, mo, dy, hr, mi, sc));
+    return 1;
+}
 
 int caltojul_cb(int argc, char *argv[])
 {
@@ -74,7 +141,6 @@ int caltojul_cb(int argc, char *argv[])
     double sc;
     int da;
     char *fmt;
-    double j;
 
     /* Ensure minimum command line */
     if (argc < 7) {
@@ -131,10 +197,7 @@ int caltojul_cb(int argc, char *argv[])
     }
 
     /* Send result */
-    if ( !tm_caltojul(yr, mo, dy, hr, mi, sc, &j) ) {
-	return 0;
-    }
-    printf(fmt, j);
+    printf(fmt, tm_caltojul(yr, mo, dy, hr, mi, sc));
     return 1;
 }
 
