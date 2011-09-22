9f7f70bba81d5e4d9c73d24e56f62f36fbbf3bfa
Gordon Carrie <dev0@trekix.net>
2011-09-22 16:47:47 +0000

    Added copyright notice

--- src/tm_calc_lib.c
+++ src/tm_calc_lib.c
@@ -2,10 +2,32 @@
    -	tm_calc_lib.c --
    -		This file defines functions for manipulate date and times
    -	
-   .	Copyright (c) 2009 Gordon D. Carrie
-   .	All rights reserved
+   .	Copyright (c) 2011, Gordon D. Carrie. All rights reserved.
+   .	
+   .	Redistribution and use in source and binary forms, with or without
+   .	modification, are permitted provided that the following conditions
+   .	are met:
+   .	
+   .	    * Redistributions of source code must retain the above copyright
+   .	    notice, this list of conditions and the following disclaimer.
    .
-   .	Please send feedback to user0@tkgeomap.org
+   .	    * Redistributions in binary form must reproduce the above copyright
+   .	    notice, this list of conditions and the following disclaimer in the
+   .	    documentation and/or other materials provided with the distribution.
+   .	
+   .	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   .	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   .	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   .	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   .	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   .	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   .	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   .	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   .	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   .	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   .	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   .
+   .	Please send feedback to dev0@trekix.net
    .
    .	Reference for calendar computations:
    .		Henry F. Fliegel and Thomas C. Van Flandern
@@ -14,7 +36,7 @@
    .		Communications of the ACM archive
    .		Volume 11 , Issue 10 (October 1968) Page: 657 ISSN:0001-0782 
    .
-   .	$Revision: 1.9 $ $Date: 2009/10/07 17:06:48 $
+   .	$Revision: 1.10 $ $Date: 2009/11/04 21:09:15 $
  */
 
 #include <limits.h>
