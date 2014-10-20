86a36c8b46d7ef577ae2ecabbcb865318b479734
Gordon Carrie <dev0@trekix.net>
2014-10-20 22:54:51 +0000

    (bug fix) Tm_JulToCal checks for 60 seconds, 60 minutes, or 24 hours in result and adjusts. Checks for and quietly skips NULL pointers in argument list.

--- src/tm_calc_lib.c
+++ src/tm_calc_lib.c
@@ -36,7 +36,7 @@
    .		Communications of the ACM archive
    .		Volume 11 , Issue 10 (October 1968) Page: 657 ISSN:0001-0782 
    .
-   .	$Revision: 1.13 $ $Date: 2012/11/08 21:16:43 $
+   .	$Revision: 1.14 $ $Date: 2013/03/18 17:23:31 $
  */
 
 #include <limits.h>
@@ -53,13 +53,16 @@ double Tm_CalToJul(int year, int month, int day,
 	+ day - 32075 + hour / 24.0 + minute / 1440.0 + second / 86400.0 - 0.5;
 }
 
-int Tm_JulToCal(double julday, int *year, int *month,
-	int *day, int *hour, int *minute, double *second)
+int Tm_JulToCal(double julday, int *year_p, int *month_p,
+	int *day_p, int *hour_p, int *minute_p, double *second_p)
 {
     double iday, fday;		/* Integer part, fraction of julday */
     double ihour, fhour;	/* Integer part, fraction of hour */
     double imin, fmin;		/* Integer part, fraction of minute */
     int l, n, i, j;		/* Intermediaries */
+    int year, month, day;	/* Hold values for year_p, month_p, day_p */
+    int hour, minute;		/* Hold values for hour_p, minute_p */
+    double second;		/* Hold value for second_p */
 
     julday += 0.5;
     fday = modf(julday, &iday);
@@ -73,16 +76,48 @@ int Tm_JulToCal(double julday, int *year, int *month,
     i = (4000 * (l + 1)) / 1461001;
     l = l - (1461 * i) / 4 + 31;
     j = (80 * l) / 2447;
-    *day = l - (2447 * j) / 80;
+    day = l - (2447 * j) / 80;
     l = j / 11;
-    *month = j + 2 - (12 * l);
-    *year = 100 * (n - 49) + i + l;
+    month = j + 2 - (12 * l);
+    year = 100 * (n - 49) + i + l;
 
     fhour = modf(fday * 24, &ihour);
     fmin = modf(fhour * 60, &imin);
-    *hour = ihour;
-    *minute = imin;
-    *second = fmin * 60;
-
+    hour = ihour;
+    minute = imin;
+    second = fmin * 60;
+    if ( second == 60.0 ) {
+	second = 0.0;
+	minute++;
+    }
+    if ( minute == 60 ) {
+	minute = 0;
+	hour++;
+    }
+    if ( hour == 24 ) {
+	hour = 0;
+	if ( !Tm_JulToCal(julday + 1, &year, &month, &day, NULL, NULL, NULL) ) {
+	    fprintf(stderr, "Could not adjust day.\n");
+	    return 0;
+	}
+    }
+    if ( year_p ) {
+	*year_p = year;
+    }
+    if ( month_p ) {
+	*month_p = month;
+    }
+    if ( day_p ) {
+	*day_p = day;
+    }
+    if ( hour_p ) {
+	*hour_p = hour;
+    }
+    if ( minute_p ) {
+	*minute_p = minute;
+    }
+    if ( second_p ) {
+	*second_p = second;
+    }
     return 1;
 }
